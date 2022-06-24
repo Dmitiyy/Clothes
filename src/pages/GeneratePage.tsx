39fd8e7e0f74bb04ddc012f5f897a97628809bdb
Dmitriy <dmitriyy311@gmail.com>
2022-06-24 20:31:02 +0300

    working on the prev arrow

--- src/pages/GeneratePage.tsx
+++ src/pages/GeneratePage.tsx
@@ -1,6 +1,7 @@
 import { FC, useEffect, useState } from "react";
 import { useAppSelector } from "../redux";
 import { useDispatch } from "react-redux";
+import { motion } from "framer-motion";
 
 import { GenerateButton } from "../components/GenerateButton";
 import { AppDispatch } from "../redux";
@@ -20,9 +21,12 @@ const GeneratePage: FC = () => {
 
   const findStepData = () => Object.entries(data).find((item) => item[1].step === stepNum)!;
   const findParamName = () => Object.entries(params).find(item => item[0] === findStepData()[0])!;
-  
-  const increaseStep = async () => {
-    setStepNum(prev => prev + 1);
+
+  const manageStep = async (plus: boolean) => {
+    if (!plus) {
+      dispatch(setDataDefault({ini: 'params', data: {...params, [findStepData()[0]]: ''}}));
+    }
+    setStepNum(prev => plus ? prev + 1 : prev - 1);
     await dispatch(fetchGenerateStep({...params})).unwrap();
   }
 
@@ -31,39 +35,39 @@ const GeneratePage: FC = () => {
       <h2>Generate clothes</h2>
       <hr />
       <h3>Step {stepNum}: <span>{findStepData()[1].description}</span></h3>
-      <div className="generate__options">
-      {
-        findStepData()[1].options?.map((item, index) => {
-          return (
-            <GenerateButton 
-              stepNum={stepNum}
-              checkbox={stepNum === 4 ? true : false} 
-              key={item} 
-              activeOption={() => handleActiveOption(index)} 
-              activeClass={index === activeOption ? true : false} 
-              title={item}
-              stepName={findStepData()[0]}
-            />
-          )
-        })
-      }
-      </div>
+      <motion.div className="generate__options">
+        {
+          findStepData()[1].options?.map((item, index) => {
+            return (
+              <GenerateButton 
+                stepNum={stepNum}
+                checkbox={stepNum === 4 ? true : false} 
+                key={item} 
+                activeOption={() => handleActiveOption(index)} 
+                activeClass={index === activeOption ? true : false} 
+                title={item}
+                stepName={findStepData()[0]}
+              />
+            )
+          })
+        }
+      </motion.div>
       <hr />
       {
         findParamName()[1].length !== 0 ? (
-          <div className="generate-next__wrap">
-            <button className="generate-prev" disabled={stepNum < 2}>
+          <motion.div className="generate-next__wrap">
+            <button className="generate-prev" disabled={stepNum < 2} onClick={() => {manageStep(false)}}>
               <svg width="31" height="24" viewBox="0 0 31 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <path d="M2 10.5C1.17157 10.5 0.5 11.1716 0.5 12C0.5 12.8284 1.17157 13.5 2 13.5V10.5ZM30.0607 13.0607C30.6464 12.4749 30.6464 11.5251 30.0607 10.9393L20.5147 1.3934C19.9289 0.807611 18.9792 0.807611 18.3934 1.3934C17.8076 1.97919 17.8076 2.92893 18.3934 3.51472L26.8787 12L18.3934 20.4853C17.8076 21.0711 17.8076 22.0208 18.3934 22.6066C18.9792 23.1924 19.9289 23.1924 20.5147 22.6066L30.0607 13.0607ZM2 13.5L29 13.5V10.5L2 10.5V13.5Z" fill="white"/>
               </svg>
             </button>
-            <button className="generate-next" onClick={increaseStep}>
+            <button className="generate-next" disabled={stepNum === 4} onClick={() => {manageStep(true)}}>
               <svg width="31" height="24" viewBox="0 0 31 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <path d="M2 10.5C1.17157 10.5 0.5 11.1716 0.5 12C0.5 12.8284 1.17157 13.5 2 13.5V10.5ZM30.0607 13.0607C30.6464 12.4749 30.6464 11.5251 30.0607 10.9393L20.5147 1.3934C19.9289 0.807611 18.9792 0.807611 18.3934 1.3934C17.8076 1.97919 17.8076 2.92893 18.3934 3.51472L26.8787 12L18.3934 20.4853C17.8076 21.0711 17.8076 22.0208 18.3934 22.6066C18.9792 23.1924 19.9289 23.1924 20.5147 22.6066L30.0607 13.0607ZM2 13.5L29 13.5V10.5L2 10.5V13.5Z" fill="white"/>
               </svg>
             </button>
-          </div>
-        ) : null
+          </motion.div>
+        ) : null 
       }
     </div>
   )
