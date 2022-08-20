74d39609a7541d924c01de05446b273992d8d378
Dmitriy <dmitriyy311@gmail.com>
2022-08-20 22:08:43 +0300

    added markup on info page

--- src/pages/InfoPage.tsx
+++ src/pages/InfoPage.tsx
@@ -1,6 +1,37 @@
+import ReactPlayer from 'react-player';
+import videoFirst from '../images/1-edit.mp4';
+import videoSecond from '../images/2-edit.mp4';
+
 const InfoPage = () => {
   return (
-    <h1>InfoPage</h1>
+    <div className="info">
+      <section className="info__videos">
+        <div className="info__videos-item">
+          <div className="info__videos-head">
+            <h2>The main idea</h2>
+            {/* <div>{Array(3).fill(1).map((_, index) => (<span key={index} />))}</div> */}
+          </div>
+          <div className="info__videos-player">
+            <ReactPlayer url={[{src: videoFirst, type: 'video/mp4'}]} width='100%' controls={true} />
+          </div>
+        </div>
+        <div className="info__videos-item">
+          <div className="info__videos-head">
+            <h2>Features</h2>
+            {/* <div>{Array(3).fill(1).map((_, index) => (<span key={index} />))}</div> */}
+          </div>
+          <div className="info__videos-player">
+            <ReactPlayer url={[{src: videoSecond, type: 'video/mp4'}]} width='100%' controls={true} />
+          </div>
+        </div>
+      </section>
+      <section className="info__description">
+        <p>
+          You can watch these videos in order to comprehend the basic concept 
+          of the project. Enjoy watching!
+        </p>
+      </section>
+    </div>
   )
 }
 export default InfoPage;
--- tasks.ts
+++ tasks.ts
@@ -1,14 +1,14 @@
 //? Todo list
 
 //* Main section
-//TODO 1. Create a "Generate page" and connect it with API
-//TODO 2. Write a google auth in our server
-//TODO 3. Create a markup of auth 
-//TODO 4. Connect client and server auth via API
-//TODO 5. Create a hook, which gets the user data
-//TODO 6. Connect home page with API
-//TODO 7. Create a seperated clothing page
-//TODO 8. Create a profile page
+//// Create a "Generate page" and connect it with API
+//// Write a google auth in our server
+//// Create a markup of auth 
+//// Connect client and server auth via API
+//// Create a hook, which gets the user data
+//// Connect home page with API
+//// Create a seperated clothing page
+//// Create a profile page
 
 //* Secondary section
 //TODO 1. Create a design and markup of doc page
